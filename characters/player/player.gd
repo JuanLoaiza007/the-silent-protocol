c5f55dfcfa1bec230f53398ee3b6e1cd13889f80
JuanLoaiza007 <juandaloaiza723@gmail.com>
2025-11-16 00:57:26 -0500

    feat: Add player running with Shift key

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const RUN_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
 const CAMERA_SENSIBILITY = 0.4
@@ -34,11 +35,14 @@ func movement(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("KEY_A", "KEY_D", "KEY_W", "KEY_S")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var speed = SPEED
+	if Input.is_key_pressed(KEY_SHIFT):
+		speed = RUN_SPEED
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	
