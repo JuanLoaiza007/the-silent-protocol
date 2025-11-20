46a47ad8b35548b0b0959a0fe48f2a4efb0a6016
JuanLoaiza007 <juandaloaiza723@gmail.com>
2025-11-19 19:55:29 -0500

    hotfix: remove add_to_group() conflicts

--- levels/level_1/level_1.gd
+++ levels/level_1/level_1.gd
@@ -3,19 +3,7 @@ extends Node3D
 const DEATH_HEIGHT = -50.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Level1World/Terrain/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_001/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_002/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_003/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_004/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_005/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_006/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_007/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_008/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platform_009/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platforms/StaticBody3D.add_to_group("grass_surface")
-	$Level1World/Platforms/StaticBody3D.add_to_group("grass_surface")
+func _ready() -> void:	
 	pass # Add any initialization logic here if needed
 
 func _physics_process(delta: float) -> void:
